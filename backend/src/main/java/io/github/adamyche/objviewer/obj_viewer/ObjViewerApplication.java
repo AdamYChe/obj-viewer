3011310c3aa452fc5c5b99fa6e40ffd73c8ce52e
AdamYChe <adamyc2018@gmail.com>
2025-05-30 14:32:32 -0400

    Setup spring project.

---
+++ backend/src/main/java/io/github/adamyche/objviewer/obj_viewer/ObjViewerApplication.java
@@ -0,0 +1,13 @@
+package io.github.adamyche.objviewer.obj_viewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjViewerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/adamyche/objviewer/obj_viewer/ObjViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.adamyche.objviewer.obj_viewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
