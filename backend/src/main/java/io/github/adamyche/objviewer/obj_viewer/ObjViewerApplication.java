5f4d0af8271b470d89ac24fb260e0d8ea84e1228
AdamYChe <adamyc2018@gmail.com>
2025-05-30 16:10:52 -0400

    Ensure Maven and Java are installed properly to display Hello World

--- backend/src/main/java/io/github/adamyche/objviewer/obj_viewer/ObjViewerApplication.java
+++ backend/src/main/java/io/github/adamyche/objviewer/obj_viewer/ObjViewerApplication.java
@@ -2,9 +2,17 @@ package io.github.adamyche.objviewer.obj_viewer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ObjViewerApplication {
+
+	@GetMapping("/hello")
+	public String check() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ObjViewerApplication.class, args);
